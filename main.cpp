67c2b6ad8e2ee6f64b4f33fc38e926a709204651
Lai-YT <381xvmvbib@gmail.com>
2024-06-02 18:19:17 +0800

    docs: bump version number

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ error_t ParseOpt(int opt, char* arg, struct argp_state* state);
 }  // namespace
 
 extern "C" {  // For argp.
-const char* argp_program_version = "sdb v1.0.1";
+const char* argp_program_version = "sdb v1.0.2";
 }
 
 int main(int argc, char* argv[]) {
